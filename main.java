cd3e6922033f084d41a44a819a8a1f47b861524e
VKCav <61792413+VKCav@users.noreply.github.com>
2020-03-31 17:26:08 +0100

    change directly in Github

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ class Main {
   System.out.println("this is NOT atom!");
   System.out.println("this is ECLIPSE!!");
   System.out.println("check this out, Eclipse change number 2!");
+  System.out.println("check this out, Eclipse change number 3 but from Github!");
   }
 }
