35997fca74ab4d8cae8857d8f2de8e33f484c77d
VKCav <61792413+VKCav@users.noreply.github.com>
2020-04-01 09:54:42 +0100

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,6 @@ class Main {
   System.out.println("this is ECLIPSE!!");
   System.out.println("check this out, Eclipse change number 2!");
   System.out.println("check this out, Eclipse change number 3!");
-  System.out.println("check this out, Github web change number 4004!");
+  System.out.println("Is this working as a pull request? Github web change number 4004!");
   }
 }
